068ffdc3b218e7e92b808f16e28d27a1086726e7
gustavo.paes <gustavo910@gmail.com>
2024-11-10 15:42:57 -0300

    feat: ajustando os itens de produto

--- app/src/main/java/br/com/alura/aluvery/MainActivity.kt
+++ app/src/main/java/br/com/alura/aluvery/MainActivity.kt
@@ -8,13 +8,17 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -25,8 +29,11 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import br.com.alura.aluvery.ui.theme.AluveryTheme
 import br.com.alura.aluvery.ui.theme.Purple200
 import br.com.alura.aluvery.ui.theme.Purple500
@@ -48,34 +55,59 @@ class MainActivity : ComponentActivity() {
 @Preview
 @Composable
 fun productItem() {
-    Column(
-        Modifier
-            .height(250.dp)
-            .width(250.dp)
-            .background(Color.White)) {
-        Box(
-            modifier = Modifier
-                .height(100.dp)
-                .fillMaxWidth()
-                .background(
-                    brush = Brush.horizontalGradient(
-                        colors = listOf(
-                            Purple500, Teal200
+    Surface(
+        shape = RoundedCornerShape(15.dp),// O RoundedCornerShape ajuda a manter a borda
+        elevation = 4.dp// mantem a borda
+    ) {
+        //Colocamos tudo dentro do surface, pois ele tem uma gama maior de meétodos de personalização
+        Column(
+            Modifier
+                .heightIn(250.dp, 300.dp) // altura minima e maxima
+                .width(250.dp)
+                .background(Color.White)
+        ) {
+            val imageSize=100.dp;
+            Box(
+                modifier = Modifier
+                    .height(imageSize)
+                    .fillMaxWidth()
+                    .background(
+                        brush = Brush.horizontalGradient(
+                            colors = listOf(
+                                Purple500, Teal200
+                            )
                         )
                     )
-                )
-        ) {
-        }
-        Image(
-            painter = painterResource(id = R.drawable.ic_launcher_background),
-            contentDescription = "deixa aqui a descrição da imagem do produto",
-            Modifier.size(100.dp)// é bom usar o size quando altura e largura são do mesmo tamanho
-                .offset( y=(-50).dp)
-                .clip(shape = CircleShape)
-                .align(Alignment.CenterHorizontally) //existem algusn metodos do modifier que saõ disponovel e  outros que não, dependendo do escopo/componente
+            ) {
 
-        )
-        Text("Texto 01!")
-        Text("Texto 012!")
+                Image(
+                    painter = painterResource(id = R.drawable.ic_launcher_background),
+                    contentDescription = "deixa aqui a descrição da imagem do produto",
+                    Modifier
+                        .size(imageSize)// é bom usar o size quando altura e largura são do mesmo tamanho
+                        .offset(y = imageSize/2)
+                        .clip(shape = CircleShape)
+                        .align(Alignment.BottomCenter) //existem algusn metodos do modifier que saõ disponovel e  outros que não, dependendo do escopo/componente
+
+                )
+            }
+            Spacer(modifier = Modifier.height(imageSize/2)) // esse compose é só para manter um espacamento
+            Column(Modifier.padding(16.dp)) {
+                Text(
+                    "Texto 01 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum!",
+//              Modifier.padding(16.dp),
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.W700,
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis
+                )
+                Text(
+                    "R$ 14,99",
+                    Modifier.padding(top = 8.dp),
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight.W500
+                )
+            }
+        }
     }
 }
