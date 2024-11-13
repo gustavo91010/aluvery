5ad7167bee78d02d1f9f543a9b6b3d40440d6681
gustavo.paes <gustavo910@gmail.com>
2024-11-12 23:55:39 -0300

    firts commit

--- app/src/main/java/br/com/alura/aluvery/ui/components/ProductItem.kt
+++ app/src/main/java/br/com/alura/aluvery/ui/components/ProductItem.kt
@@ -2,18 +2,10 @@ package br.com.alura.aluvery.ui.components
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.heightIn
-import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -21,7 +13,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -31,68 +22,62 @@ import androidx.compose.ui.tooling.preview.datasource.LoremIpsum
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import br.com.alura.aluvery.R
-import br.com.alura.aluvery.extentions.toBrazilianCurrency
+import br.com.alura.aluvery.extensions.toBrazilianCurrency
 import br.com.alura.aluvery.model.Product
-import br.com.alura.aluvery.ui.theme.Purple500
-import br.com.alura.aluvery.ui.theme.Teal200
+import br.com.alura.aluvery.ui.theme.AluveryTheme
 import java.math.BigDecimal
 
 @Composable
-fun productItem(product: Product) {
+fun ProductItem(product: Product) {
     Surface(
-        // Modifier.padding(8.dp),//espaço entre componentes
-        shape = RoundedCornerShape(15.dp),// O RoundedCornerShape ajuda a manter a borda
-        elevation = 4.dp// mantem a borda
+        shape = RoundedCornerShape(15.dp),
+        elevation = 4.dp
     ) {
-        //Colocamos tudo dentro do surface, pois ele tem uma gama maior de meétodos de personalização
         Column(
             Modifier
-                .heightIn(250.dp, 300.dp) // altura minima e maxima
-                .width(250.dp)
-                .background(Color.White)
+                .heightIn(250.dp, 300.dp)
+                .width(200.dp)
         ) {
-            val imageSize = 100.dp;
+            val imageSize = 100.dp
             Box(
                 modifier = Modifier
                     .height(imageSize)
-                    .fillMaxWidth()
                     .background(
                         brush = Brush.horizontalGradient(
                             colors = listOf(
-                                Purple500, Teal200
+                                MaterialTheme.colors.primary,
+                                MaterialTheme.colors.secondary
                             )
                         )
                     )
+                    .fillMaxWidth()
             ) {
-
                 Image(
-                    painter = painterResource(id = product.image),
-//                    painter = painterResource(id = R.drawable.ic_launcher_background),
-                    contentDescription = "deixa aqui a descrição da imagem do produto",
+                    // TODO: ajustar imagem do produto
+                    painter = painterResource(id = R.drawable.placeholder),
+                    contentDescription = null,
                     Modifier
-                        .size(imageSize)// é bom usar o size quando altura e largura são do mesmo tamanho
+                        .size(imageSize)
                         .offset(y = imageSize / 2)
                         .clip(shape = CircleShape)
-                        .align(Alignment.BottomCenter), //existem algusn metodos do modifier que saõ disponovel e  outros que não, dependendo do escopo/componente
-                    contentScale = ContentScale.Crop // ele não diminui a imagem tentando por ela no espaço selecioando, ele coloca o que da pra ver pelo esapço
-//                            contentScale = ContentScale.Fit // por padrão ele usa o fit, que é incaixando o eapço
+                        .align(Alignment.BottomCenter),
+                    contentScale = ContentScale.Crop
                 )
             }
-            Spacer(modifier = Modifier.height(imageSize / 2)) // esse compose é só para manter um espacamento
+            Spacer(modifier = Modifier.height(imageSize / 2))
             Column(Modifier.padding(16.dp)) {
                 Text(
-                    product.name,
-//              Modifier.padding(16.dp),
+                    text = product.name,
                     fontSize = 18.sp,
-                    fontWeight = FontWeight.W700,
+                    fontWeight = FontWeight(700),
                     maxLines = 2,
                     overflow = TextOverflow.Ellipsis
                 )
                 Text(
-                    product.price.toBrazilianCurrency(),
+                    text = product.price.toBrazilianCurrency(),
                     Modifier.padding(top = 8.dp),
                     fontSize = 14.sp,
-                    fontWeight = FontWeight.W500
+                    fontWeight = FontWeight(400)
                 )
             }
         }
@@ -101,12 +86,15 @@ fun productItem(product: Product) {
 
 @Preview(showBackground = true)
 @Composable
-private fun productItemPreview() {
-    productItem(
-        Product(
-            name = LoremIpsum(50).values.first(),
-            price = BigDecimal("15.78"),
-            image = R.drawable.placeholder
-        )
-    )
+private fun ProductItemPreview() {
+    AluveryTheme {
+        Surface {
+            ProductItem(
+                Product(
+                    name = LoremIpsum(50).values.first(),
+                    price = BigDecimal("14.99")
+                )
+            )
+        }
+    }
 }
